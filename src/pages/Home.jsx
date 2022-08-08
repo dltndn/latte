0604ab0807a443883f8235a2b6e03bd65ec8ed2e
dltndn <james98099@gmail.com>
2022-08-08 21:24:25 +0900

    home page css 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,28 @@ import styled from "styled-components";
 
 const Home = () => {
     return(<div>
-        <div>리더보드</div>
-        <div>로그인창</div>
+        <Page>
+            <div>요약리더보드</div>
+            <div>로그인창</div>
+        </Page>
     </div>);
 }
 
+//로그인 창 css
+const LogInBox = styled.div`
+    
+`;
+
+const LeaderboardSummary = styled.div`
+    
+`;
+
+const Page = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+`;
 
 //input data
 //승점 상위 3인 승점 data
--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -3,12 +3,14 @@ import styled from "styled-components";
 const LeaderBoard = () => {
     return(<div>
         <div>리더보드 상세페이지</div>
-        
+        <div>상위리그</div>
+        <div>하위리그</div>
     </div>);
 }
 
 // input data
 // player 정보
+// player 소속 리그
 
 // 승점에 따른 순서 배열 
 
