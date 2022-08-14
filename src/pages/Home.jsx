5f78ed8b689b20649c8be61b10c15b975a4cb77a
dltndn <james98099@gmail.com>
2022-08-14 23:14:00 +0900

    feat: LogInBtn onclick 이벤트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,43 +2,56 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [viewLoginBox, setViewLoginBox] = useState(false);
 
-    const [viewLoginBox, setViewLoginBox] = useState(true);
+//   useEffect(() => {
+//     console.log("useeffect")
+//   }, []  );
 
-    return(<div>
-        <Page>
-            <LeaderboardSummary>요약리더보드</LeaderboardSummary>
+  const getLogInBox = () => {
+    setViewLoginBox(true);
+  }
+
+  return (
+    <div>
+      <Page>
+        <LeaderboardSummary>요약리더보드</LeaderboardSummary>
+        <div>
+          {viewLoginBox ? (
+            <LogInBox></LogInBox>
+          ) : (
             <div>
-                { viewLoginBox ? <LogInBox/> : <EnterButton />}
+              <LogInBtn onClick={getLogInBox}>로그인하기</LogInBtn>
+              <GuestBtn>둘러보기</GuestBtn>
             </div>
-        </Page>
-    </div>);
-}
+          )}
+        </div>
+      </Page>
+    </div>
+  );
+};
 
-const EnterButton = styled.div`
-    
-`;
+const LogInBtn = styled.button``;
+
+const GuestBtn = styled.button``;
+
+const EnterButton = styled.div``;
 
 //로그인 창 css
-const LogInBox = styled.div`
-    
-`;
+const LogInBox = styled.div``;
 
-const LeaderboardSummary = styled.div`
-    
-`;
+const LeaderboardSummary = styled.div``;
 
 const Page = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 //input data
 //승점 상위 3인 승점 data
 
 // 승점 상위 3인 노출
-
 
 export default Home;
